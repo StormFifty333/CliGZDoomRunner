172e4ff0e7848acaea2918045f077db8ecb3f945
StormFifty333 <105613484+StormFifty333@users.noreply.github.com>
2025-03-04 10:19:49 -0600

    more things

--- gzdoomrunner.bat
+++ gzdoomrunner.bat
@@ -6,14 +6,32 @@ IF NOT EXIST "%appdata%\GZDoom" (
 	MKDIR "%appdata%\GZDoom"
 	ECHO [33m[INFO] GZDoom folder created in %appdata%[0m
 ) ELSE (
-	ECHO [33m[INFO] GZDoom folder already exists...[0m
+	ECHO [33m[INFO] GZDoom folder already exists![0m
 )
 
-:downloads gzdoom from the internet
-ECHO [33m[INFO] Attempting gzdoom-4-14-0a-windows.zip download...[0m
-curl -L -O https://github.com/ZDoom/gzdoom/releases/download/g4.14.0/gzdoom-4-14-0a-windows.zip
-ECHO [33m[INFO] gzdoom-4-14-0a-windows.zip download complete![0m
+:checks for existing gzdoom exe
+if NOT EXIST "%appdata%\GZDoom\gzdoom.exe" (
 
-MOVE gzdoom-4-14-0a-windows.zip "%appdata%\GZDoom"
+	:downloads gzdoom from the internet
+	if NOT EXIST "%appdata%\GZDoom\gzdoom-4-14-0a-windows.zip" (
+		ECHO [33m[INFO] Attempting gzdoom-4-14-0a-windows.zip download...[0m
+		curl -L -O https://github.com/ZDoom/gzdoom/releases/download/g4.14.0/gzdoom-4-14-0a-windows.zip
+		ECHO [33m[INFO] gzdoom-4-14-0a-windows.zip download complete![0m
+		MOVE gzdoom-4-14-0a-windows.zip "%appdata%\GZDoom"
+	) ELSE (
+		ECHO [33m[INFO] gzdoom-4-14-0a-windows.zip already exists![0m
+	)
+	
+	:extracts gzdoom zip
+	ECHO [33m[INFO] Beginning zip extraction...[0m
+	powershell Expand-Archive "%appdata%\GZDoom\gzdoom-4-14-0a-windows.zip" "%appdata%\GZDoom"
+	ECHO [33m[INFO] Extraction complete![0m
+	
+	:removes zip
+	DEL "%appdata%\GZDoom\gzdoom-4-14-0a-windows.zip"
+	
+) ELSE (
+	ECHO [33m[INFO] Existing install found! No corruption free guarantee![0m
+)
 
 PAUSE
