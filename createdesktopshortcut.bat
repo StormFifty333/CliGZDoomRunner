4db438ab07b67441f2b30ae42a88dd756216a065
StormFifty333 <105613484+StormFifty333@users.noreply.github.com>
2025-05-08 10:20:05 -0500

    Update createdesktopshortcut.bat

--- createdesktopshortcut.bat
+++ createdesktopshortcut.bat
@@ -4,4 +4,4 @@ SETLOCAL enabledelayedexpansion
 
 for /f "usebackq tokens=1,2,*" %%B IN (`reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop`) do set DESKTOP=%%D
 
-MKLINK "%DESKTOP%\GZDoom Runner.lnk" "%localappdata%\GZDoom\gzdoomrunner.bat"
+MKLINK "%DESKTOP%\GZDoom Runner" "%localappdata%\GZDoom\gzdoomrunner.bat"
